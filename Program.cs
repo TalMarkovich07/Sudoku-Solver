9da6209ee1e00ebf8c3f92ae92730d97c727253e
Marko <markovich.tal@gmail.com>
2026-01-14 20:48:07 +0200

    created exceptions cs and added general and wrong length exceptions

--- Program.cs
+++ Program.cs
@@ -10,6 +10,11 @@ namespace Sudoku_Solver
     internal class Program
     {
         public static int MaxSize = 9;
+        public static int[][] ToMat(string input)
+        {
+            if (input.Length != Math.Pow(MaxSize, 2))
+                throw new SudokuException($"The board must be {Program.MaxSize}x{Program.MaxSize}.");
+        }
         static void Main(string[] args)
         {
         }
